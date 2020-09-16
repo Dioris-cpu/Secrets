e2e2f0e5a6cfa74f5ce4d5f8da0d85a4b5cf7d83
Dioris Diaz <diorid@gmail.com>
2020-09-16 18:57:30 -0400

    environment variables

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const encrypt = require('mongoose-encryption');
+const { Console } = require('console');
 
 const app = express();
+console.log(process.env.API_KEY);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -23,8 +26,8 @@ const userSchema = new mongoose.Schema ( {
 });
 
 // encryption
-const secret = 'Thisisourlittlesecret'
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']  })
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  })
 
 const User = new mongoose.model('User', userSchema)
 
