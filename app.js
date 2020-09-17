d6a3cd5c586fdc5e1a192f747a27933cbcca2cd2
Dioris Diaz <diorid@gmail.com>
2020-09-16 21:36:41 -0400

    hashing passwords using md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5')
 const { Console } = require('console');
 
 const app = express();
@@ -25,9 +25,6 @@ const userSchema = new mongoose.Schema ( {
     password: String
 });
 
-// encryption
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  })
 
 const User = new mongoose.model('User', userSchema)
 
@@ -47,7 +44,8 @@ app.get('/register', function(req, res){
 app.post('/register', function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        // hash password thru md5
+        password: md5(req.body.password)
     })
     // save new users
     newUser.save(function(err){
@@ -61,7 +59,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
     // check if email and password match
     User.findOne({email: username}, function(err, foundUser){
         if(err){
